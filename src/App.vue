bcb5bba84fa883e1ea716bf4309f9756acca7b59
Alek30k <alemank23k@gmail.com>
2023-02-05 11:25:25 -0300

    last commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <script setup>
 const name = "Vue dinámico";
 const styleColor = "color: blue";
-const arrayFrutas = ["🍎", "🍏", "🍍", "🍎", "🍏", "🍍"];
+// const arrayFrutas = ["🍎", "🍏", "🍍", "🍎", "🍏", "🍍"];
+const arrayFrutas = [
+  {
+    name: "Manzana",
+    price: "$1.00",
+    desc: "Una manzana",
+  },
+  {
+    name: "Pera",
+    price: "$2.00",
+    desc: "Una pera",
+  },
+  {
+    name: "Naranja",
+    price: "$3.00",
+    desc: "Una naranja",
+  },
+];
 </script>
 
 <template>
   <h1>Hello {{ name.toUpperCase() }}</h1>
   <h2 :style="styleColor">Soy azul</h2>
   <ul>
-    <li v-for="(fruta, i) in arrayFrutas" :key="i">{{ i }} - {{ fruta }}</li>
+    <li v-for="fruta in arrayFrutas" :key="name">
+      {{ fruta.name }}
+      {{ fruta.price }}
+      {{ fruta.desc }}
+    </li>
   </ul>
 </template>
 
